91a69280b2cae9b2fc160e147d72525cf9add395
moicano <moicano@debian>
2021-10-19 19:22:59 -0300

    exerc01

---
+++ 01.py
@@ -0,0 +1,11 @@
+nome = input("Nome: ")
+cpf = int(input("CPF: "))
+idade = int(input("Idade: "))
+
+print("---------------------------")
+print("Confirmação de Cadastro:")
+print("Nome: {}".format(nome))
+print("CPF: {}".format(cpf))
+print("Idade: {}".format(idade))
+
+print("---------------------------")
